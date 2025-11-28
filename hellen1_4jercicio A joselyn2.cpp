a4c56152f794e00acd9a19c9906ad64121793205
hellenmaitazerpa-byte <hellenmaitazerpa@gmail.com>
2025-11-28 09:58:12 -0400

    Add files via upload

---
+++ hellen1_4jercicio A joselyn2.cpp
@@ -0,0 +1,24 @@
+// programa de 4
+#include <iostream>
+using namespace std;
+int main() {
+   int opcion;
+   int BS;
+   int EUROS;
+  cout<<" Menu de convercion"<<endl;
+  cout<<"1)ingrese lo que desea convertir de BS a EUROS:";
+  cin>> BS;
+  cout<<"2)ingrese lo que desea cambiar a EUROS a BS:";
+  cin>> EUROS;
+
+  switch(opcion){
+    case 1: cout<<"BS A EUROS"; break;
+    case 2: cout<<"EUROS A BS"; break;
+    
+    
+  }
+
+    return 0;
+}
+
+
